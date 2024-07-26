d000d865e28e76c6f1195ade742f5e178fd55a48
shakefishes <crimson2357@gmail.com>
2024-07-26 20:16:02 +0900

    add:create.blade_新規作成画面作成
    feat:TodoController,index.blade,web_Todoを追加ボタン及びルート定義

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -15,5 +15,9 @@ class TodoController extends Controller
 
         return view('todo.index', ['todos' => $todos]);
     }
-}
 
+    public function create()
+    {
+        return view('todo.create');
+    }
+}
