0926c970996c499999ef020bbab919ee6370066f
shakefishes <crimson2357@gmail.com>
2024-07-30 08:25:45 +0900

    feat:TodoController,Todo_フォームから送信した値の一括取得及び代入

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -23,12 +23,13 @@ class TodoController extends Controller
 
     public function store(Request $request)
     {
-        $content = $request->input('content');
-
+        $inputs = $request->all();
+        dd($inputs);
+    
         $todo = new Todo();
-        $todo->content = $content;
+        $todo->fill($inputs);
         $todo->save();
-
+    
         return redirect()->route('todo.index');
     }
 }
